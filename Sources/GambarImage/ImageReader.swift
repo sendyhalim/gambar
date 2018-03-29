e170eb7b507c3d26774db89e540d4ccce9e47dae
Sendy Halim <sendyhalim93@gmail.com>
2018-03-30 02:32:35 +0700

    Add function to return color pixels

--- Sources/GambarImage/ImageReader.swift
+++ Sources/GambarImage/ImageReader.swift
@@ -1,21 +1,27 @@
 import Foundation
 import SwiftGD
-import Swiftz
-import Operadics
 
 public enum ImageReaderError: Swift.Error {
   case imageNotFound(path: String)
 }
 
-func points(width: Int, height: Int) -> List<Point> {
-  let xs = List(fromArray: Array(0 ..< width))
-  let ys = List(fromArray: Array(0 ..< height))
+func points(width: Int, height: Int) -> Array<Point> {
+  let xs = Array(0 ..< width)
+  let ys = Array(0 ..< height)
 
-  return curry(Point.init) <^> xs <*> ys
+  return xs.flatMap { x in
+    ys.map { y in Point(x: x, y: y) }
+  }
 }
 
+func toRGB(_ colorValue: Double) -> Int {
+  return Int(colorValue * 255)
+}
+
+public typealias RGBA = (Int, Int, Int, Double)
+
 public struct ImageReader {
-  public func read(path: String) throws -> Image {
+  public static func read(path: String) throws -> Image {
     guard File.exists(path: path) else {
       throw ImageReaderError.imageNotFound(path: path)
     }
@@ -23,5 +29,20 @@ public struct ImageReader {
     let url = URL(fileURLWithPath: path)
 
     return Image(url: url)!
+  }
+
+  public static func pixels(path: String) throws -> Array<RGBA> {
+    let image = try read(path: path)
+
+    return points(width: image.size.width, height: image.size.height).map { point in
+      let color = image.get(pixel: point)
+
+      return (
+        toRGB(color.redComponent),
+        toRGB(color.greenComponent),
+        toRGB(color.blueComponent),
+        color.alphaComponent
+      )
+    }
   }
 }
