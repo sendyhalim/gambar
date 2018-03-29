511d7c78288360338bb29ffc2b3fd418bc1c29dd
Sendy Halim <sendyhalim93@gmail.com>
2018-03-30 01:10:20 +0700

    Install Swift libgd wrapper

--- Package.swift
+++ Package.swift
@@ -7,14 +7,18 @@ let package = Package(
   name: "Gambar",
   dependencies: [
     .package(url: "https://github.com/Quick/Quick", from: "1.2.0"),
-    .package(url: "https://github.com/kylef/Commander", .branch("master"))
+    .package(url: "https://github.com/kylef/Commander", .branch("master")),
+    .package(url: "https://github.com/twostraws/SwiftGD.git", .branch("master"))
   ],
   targets: [
     .target(
       name: "Gambar",
       dependencies: ["GambarImage", "Commander"]
     ),
-    .target(name: "GambarImage"),
+    .target(
+      name: "GambarImage",
+      dependencies: ["SwiftGD"]
+    ),
     .testTarget(
       name: "GambarTests",
       dependencies: ["GambarImage", "Quick", "Nimble"],
