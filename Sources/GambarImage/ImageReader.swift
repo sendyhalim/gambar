89c8e53bea335d16a405ae97d6c80030f85d248d
Sendy Halim <sendyhalim93@gmail.com>
2018-04-06 02:16:12 +0700

    Group points in matrix instead of flat array

--- Sources/GambarImage/ImageReader.swift
+++ Sources/GambarImage/ImageReader.swift
@@ -1,16 +1,19 @@
 import Foundation
 import SwiftGD
+import Swiftz
+
+public typealias RGBA = (Int, Int, Int, Double)
 
 public enum ImageReaderError: Swift.Error {
   case imageNotFound(path: String)
 }
 
-func points(width: Int, height: Int) -> Array<Point> {
+func points(width: Int, height: Int) -> Array<Array<Point>> {
   let xs = Array(0 ..< width)
   let ys = Array(0 ..< height)
 
-  return xs.flatMap { x in
-    ys.map { y in Point(x: x, y: y) }
+  return xs.map { x in
+    ys.map(curry(Point.init)(x))
   }
 }
 
@@ -18,7 +21,26 @@ func toRGB(_ colorValue: Double) -> Int {
   return Int(colorValue * 255)
 }
 
-public typealias RGBA = (Int, Int, Int, Double)
+func pixelAtPoint(image: Image, point: Point) -> RGBA {
+  let color = image.get(pixel: point)
+
+  return (
+    toRGB(color.redComponent),
+    toRGB(color.greenComponent),
+    toRGB(color.blueComponent),
+    color.alphaComponent
+  )
+}
+
+func groupPoints(by size: Int, pointMatrix: Array<Array<Point>>) -> Array<Array<Point>> {
+  let splitted = pointMatrix.map {
+    splitBy(f: { $0.y % size == 0 }, items: $0)
+  }
+
+  return splitted.dropFirst().reduce(splitted.first!) { acc, ys in
+   return zipWith(f: (+), xs: acc, ys: ys)
+  }
+}
 
 public struct ImageReader {
   public static func read(path: String) throws -> Image {
@@ -31,18 +53,13 @@ public struct ImageReader {
     return Image(url: url)!
   }
 
-  public static func pixels(path: String) throws -> Array<RGBA> {
+  public static func pixels(path: String) throws -> Array<Array<RGBA>> {
     let image = try read(path: path)
+    let imagePixel = curry(pixelAtPoint)(image)
+    let pointMatrix = points(width: image.size.width, height: image.size.height)
 
-    return points(width: image.size.width, height: image.size.height).map { point in
-      let color = image.get(pixel: point)
-
-      return (
-        toRGB(color.redComponent),
-        toRGB(color.greenComponent),
-        toRGB(color.blueComponent),
-        color.alphaComponent
-      )
+    return groupPoints(by: 3, pointMatrix: pointMatrix).map {
+      $0.map(imagePixel)
     }
   }
 }
--- Tests/GambarImageTests/ImageReaderSpec.swift
+++ Tests/GambarImageTests/ImageReaderSpec.swift
@@ -13,14 +13,18 @@ struct ImageReaderSpec {
       }
 
       $0.describe("given 0 height") {
-        $0.it("should return empty points") {
-          try expect(points(width: 10, height: 0).count) == 0
+        $0.it("should return 10 empty vertical points") {
+          try expect(points(width: 10, height: 0).count) == 10
         }
       }
 
       $0.describe("given 3 width and 2 height") {
         $0.it("should return 6 points") {
-          let _points = List(fromArray: points(width: 3, height: 2).map { ($0.x, $0.y) })
+          let _points = List(fromArray: points(width: 3, height: 2)
+            .flatMap { $0 }
+            .map { ($0.x, $0.y) }
+          )
+
           let expectation = List(fromArray: [
             (0, 0), (0, 1),
             (1, 0), (1, 1),
