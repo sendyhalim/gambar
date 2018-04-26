79cd4cb4d0562c6aa4becd3f835b2ad0c6fb208d
Sendy Halim <sendyhalim93@gmail.com>
2018-04-27 02:20:48 +0700

    Implement command line interface

--- Sources/Gambar/main.swift
+++ Sources/Gambar/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import Commander
+import GambarImage
+
+let main = command(
+  Argument<String>("path", description: "Path to image")
+) { path in
+  do {
+    let str = try ImageReader.asciiString(path: path)
+    print(str)
+  } catch {
+    print("Error \(error)")
+  }
+}
+
+main.run()
